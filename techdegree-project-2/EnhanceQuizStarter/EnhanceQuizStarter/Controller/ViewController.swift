e9ad688a1a6688d252ef3f468175e9d8fec3f903
wwCondor <45741282+wwCondor@users.noreply.github.com>
2018-12-24 15:10:32 +0100

    Add files via upload

---
+++ techdegree-project-2/EnhanceQuizStarter/EnhanceQuizStarter/Controller/ViewController.swift
@@ -0,0 +1,135 @@
+//
+//  ViewController.swift
+//  EnhanceQuizStarter
+//
+//  Created by Pasan Premaratne on 3/12/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - Outlets
+    
+    
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet weak var answerButtonOne: UIButton!
+    @IBOutlet weak var answerButtonTwo: UIButton!
+    @IBOutlet weak var answerButtonThree: UIButton!
+    @IBOutlet weak var answerButtonFour: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        PlaySounds.loadGameStartSound()
+        PlaySounds.playGameStartSound()
+        displayQuestionsAndAnswers()
+    }
+    
+    
+    let quizMaster = QuizMaster()
+    
+    // this function displays the questions and answers
+    // and hides the unsused buttons
+    func displayQuestionsAndAnswers() {
+        let questionDataFromQuiz = quizMaster.provideQuestion()
+        questionField.text = questionDataFromQuiz.question
+        answerButtonOne.setTitle(questionDataFromQuiz.answerOne, for: .normal)
+        answerButtonTwo.setTitle(questionDataFromQuiz.answerTwo, for: .normal)
+        answerButtonThree.setTitle(questionDataFromQuiz.answerThree, for: .normal)
+        answerButtonFour.setTitle(questionDataFromQuiz.answerFour, for: .normal)
+        // When question is displayed answerButtons are visible
+        playAgainButton.isHidden = true
+        answerButtonOne.isHidden = false
+        answerButtonTwo.isHidden = false
+        answerButtonThree.isHidden = false
+        answerButtonFour.isHidden = false
+    }
+    
+    /*
+    // this function hides the buttons that are not needed
+    func hideExtraAnswerButtons() {
+        let numberOfAnswers = quizMaster.retrieveNumberOfAnswers()
+        if numberOfAnswers == 3 {
+            answerButtonOne.isHidden = false
+            answerButtonTwo.isHidden = false
+            answerButtonThree.isHidden = false
+            answerButtonFour.isHidden = true
+        } else if numberOfAnswers == 2 {
+            answerButtonOne.isHidden = false
+            answerButtonTwo.isHidden = false
+            answerButtonThree.isHidden = true
+            answerButtonFour.isHidden = true
+        } else {
+            answerButtonOne.isHidden = false
+            answerButtonTwo.isHidden = false
+            answerButtonThree.isHidden = false
+            answerButtonFour.isHidden = false
+        }
+    }
+    */
+    
+    // this function checks whether game is over or if it is time for the next question
+    func nextRound() {
+        if quizMaster.questionsAsked == quizMaster.questionsPerRound {
+            // Game is over
+            displayScore()
+        } else {
+            // If questionAsked NOT equal questionsPerRound: continue game
+            displayQuestionsAndAnswers()
+        }
+    }
+    
+    func displayScore() {
+        // When score is displayed answerButtons are hidden
+        answerButtonOne.isHidden = true
+        answerButtonTwo.isHidden = true
+        answerButtonThree.isHidden = true
+        answerButtonFour.isHidden = true
+        
+        // Display play again button
+        playAgainButton.isHidden = false
+        
+        questionField.text = "Way to go!\nYou got \(quizMaster.correctQuestions) out of \(quizMaster.questionsPerRound) correct \nand \(quizMaster.wrongAnswersGiven) wrong"
+    }
+    
+    func loadNextRound(delay seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.nextRound()
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func checkTheAnswer(_ sendQuestion: UIButton) {
+        // Increment the questions asked counter
+        switch sendQuestion {
+        case answerButtonOne: questionField.text = quizMaster.checkAnswer(buttonPressed: 1)
+        case answerButtonTwo: questionField.text = quizMaster.checkAnswer(buttonPressed: 2)
+        case answerButtonThree: questionField.text = quizMaster.checkAnswer(buttonPressed: 3)
+        case answerButtonFour: questionField.text = quizMaster.checkAnswer(buttonPressed: 4)
+        default: break
+        }
+        
+        loadNextRound(delay: 2)
+    }
+    
+    
+    @IBAction func playAgain(_ sender: UIButton) {
+        // Show the answer buttons
+        quizMaster.questionsAsked = 0
+        quizMaster.correctQuestions = 0
+        quizMaster.wrongAnswersGiven = 0
+        nextRound()
+    }
+    
+    
+}
+
---
+++ techdegree-project-2/EnhanceQuizStarter/EnhanceQuizStarter/Model/QuizMaster.swift
@@ -0,0 +1,202 @@
+//
+//  QuizMaster.swift
+//  EnhanceQuizStarter
+//
+//  Created by Wouter Willebrands on 23/12/2018.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+
+class QuizMaster {
+        
+    struct TriviaQuestions {
+        let question: String
+        let answerOne: String
+        let answerTwo: String
+        let answerThree: String
+        let answerFour: String
+        let correctAnswer: Int
+        var amountOfAnswers: Int
+        
+        func retrieveAmountOfAnswers() -> Int {
+            return amountOfAnswers
+        }
+        
+    }
+    
+    let questionOne = TriviaQuestions(
+        question: "What is the correct syntax for a constant?",
+        answerOne: "const",
+        answerTwo: "var",
+        answerThree: "let",
+        answerFour: "con",
+        correctAnswer: 3,
+        amountOfAnswers: 4)
+    
+    let questionTwo = TriviaQuestions(
+        question: "What is the correct syntax for a variable",
+        answerOne: "const",
+        answerTwo: "var",
+        answerThree: "let",
+        answerFour: "con",
+        correctAnswer: 2,
+        amountOfAnswers: 4)
+    
+    let questionThree = TriviaQuestions(
+        question: "A sequence of characters surrounded by double quotes is called a",
+        answerOne: "String Literal",
+        answerTwo: "Global Variable",
+        answerThree: "Literal Expression",
+        answerFour: "This should be hidden",
+        correctAnswer: 1,
+        amountOfAnswers: 3)
+    
+    let questionFour = TriviaQuestions(
+        question: "What statement can be used to stop the execution of a loop, if, or switch statement?",
+        answerOne: "Stop",
+        answerTwo: "End",
+        answerThree: "Exit",
+        answerFour: "Break",
+        correctAnswer: 4,
+        amountOfAnswers: 4)
+    
+    let questionFive = TriviaQuestions(
+        question: "Which of the following is not a numerical type?",
+        answerOne: "Int",
+        answerTwo: "Float",
+        answerThree: "Bool",
+        answerFour: "Double",
+        correctAnswer: 3,
+        amountOfAnswers: 4)
+    
+    let questionSix = TriviaQuestions(
+        question: "What is composition?",
+        answerOne: "Designing a class through protocol conformance",
+        answerTwo: "Designing a class through inheritance",
+        answerThree: "It refers to the way you organise all your objects",
+        answerFour: "None of the above",
+        correctAnswer: 1,
+        amountOfAnswers: 4)
+    
+    let questionSeven = TriviaQuestions(
+        question: "What is the name of the 'structure' that can be prevented using a Gaurd statement",
+        answerOne: "The tower of Doom",
+        answerTwo: "The Pyramid of Danger",
+        answerThree: "The Pyramifd of Doom",
+        answerFour: "The Party-hat of Disaster",
+        correctAnswer: 2,
+        amountOfAnswers: 4)
+    
+    let questionEight = TriviaQuestions(
+        question: "Enumerations",
+        answerOne: "are very flexible",
+        answerTwo: "do not have to ptovide a value for each case",
+        answerThree: "are types in their own right",
+        answerFour: "All of the above",
+        correctAnswer: 4,
+        amountOfAnswers: 4)
+    
+    let questionNine = TriviaQuestions(
+        question: "Array, set and ditionary are the three primary collection types",
+        answerOne: "True",
+        answerTwo: "False",
+        answerThree: "This answer should be hidden",
+        answerFour: "This answer should be hidden",
+        correctAnswer: 1,
+        amountOfAnswers: 2)
+    
+    let questionTen = TriviaQuestions(
+        question: "A method is another word for a function",
+        answerOne: "False",
+        answerTwo: "True",
+        answerThree: "This answer should be hidden",
+        answerFour: "This answer should be hidden",
+        correctAnswer: 2,
+        amountOfAnswers: 2)
+    
+    let questionEleven = TriviaQuestions(
+        question: "Which of the following statements is not true: Swift...",
+        answerOne: "...is fast language and supports dynamic libraries",
+        answerTwo: "...has a high readability, is a safe platform and is open-source",
+        answerThree: "...is a high maintenance langauge and its playground encourages interactive coding",
+        answerFour: "... is fast, supports dynamic libraries and has a high readability",
+        correctAnswer: 3,
+        amountOfAnswers: 4)
+    
+    
+    // this determines the amount of questions per round
+    let questionsPerRound = 4
+    
+    // this counts the amount of questions asked
+    var questionsAsked = 0
+    
+    // this is the amount of correctly answered questions
+    var correctQuestions = 0
+    
+    // this if the amount of answers that were wrong
+    var wrongAnswersGiven = 0
+    
+    var indexOfSelectedQuestion = 0
+    
+    // this is all the combined questions
+    var questionsCombined = [TriviaQuestions]()
+    
+    // this variable is used to reorganise the questions
+    var questionsReorganised = [TriviaQuestions]()
+    
+    // this function reorganises the questionsCombined
+    func arrayReorganised() {
+        questionsReorganised = questionsCombined.shuffled()
+    }
+    
+    // this is the initializer for questionsCombined
+    init() {
+        self.questionsCombined = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen, questionEleven]
+    }
+    
+    /*
+    let triviaQuestions = [TriviaQuestions]()
+    
+    func retrieveNumberOfAnswers() -> Int {
+        let numberOfQuestions = triviaQuestions.amountOfAnswers[]
+        return numberOfQuestions
+    }
+    */
+    
+    // this selects a question from the TriviaQuestions
+    func provideQuestion() -> TriviaQuestions {
+        let questionReturned = questionsCombined[questionsAsked]
+        return questionReturned
+    }
+    
+    // this checks the answer and increases score if answer is correct
+    // or increases counter for wrong answer given
+    func checkAnswer(buttonPressed: Int) -> String {
+        if (buttonPressed == 1 &&  questionsReorganised[questionsAsked].correctAnswer == 1) {
+            correctQuestions += 1
+            questionsAsked += 1
+            return "Yay!"
+        } else if (buttonPressed == 2 &&  questionsReorganised[questionsAsked].correctAnswer == 2) {
+            correctQuestions += 1
+            questionsAsked += 1
+            return "Yay!"
+        } else if (buttonPressed == 3 &&  questionsReorganised[questionsAsked].correctAnswer == 3) {
+            correctQuestions += 1
+            questionsAsked += 1
+            return "Yay!"
+        } else if (buttonPressed == 4 &&  questionsReorganised[questionsAsked].correctAnswer == 4) {
+                correctQuestions += 1
+                questionsAsked += 1
+                return "Yay!"
+        } else {
+            questionsAsked += 1
+            wrongAnswersGiven += 1
+            return "Woops!"
+        }
+    }
+    
+    
+    
+}
